c6b525bd4b76d91978a8510ddd0a0516894a45c5
Saarim Zaman <saarim.z@gmail.com>
2018-01-15 12:42:20 -0500

    added firebase

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -3,7 +3,6 @@ import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 import ExpensesSummary from './ExpensesSummary';
 
-
 const ExpenseDashboardPage = (props) => (
     <div>
         <ExpensesSummary />
