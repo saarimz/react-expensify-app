36389382c1de5d3427ffacfd6bd3657f66acc7cd
Saarim Zaman <saarim.z@gmail.com>
2018-01-27 15:57:35 -0500

    started adding base styling

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -20,12 +20,19 @@ export class EditExpensePage extends React.Component {
     render(){
         return (
             <div>
-                <ExpenseForm 
-                    expense={this.props.expense}
-                    onSubmit={this.onSubmit}
-                    action="Edit Expense"
-                />
-                <button onClick={this.onRemove}>Remove</button>
+                <div className="page-header">
+                    <div className="content-container">
+                        <h1 className="page-header__title">Edit Expense</h1>
+                    </div>
+                </div>
+                <div className="content-container">
+                    <ExpenseForm 
+                        expense={this.props.expense}
+                        onSubmit={this.onSubmit}
+                        action="Edit Expense"
+                    />
+                    <button className="bttn bttn--secondary" onClick={this.onRemove}>Remove Expense</button>
+                </div>
             </div>
         );
     }
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -65,39 +65,42 @@ export default class ExpenseForm extends React.Component {
     }
     render(){
         return (
-            <div>
-                {!!this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
-                    <input 
-                        type="text" 
-                        placeholder="Description" 
-                        value={this.state.description}
-                        onChange = {this.onDescriptionChange}
-                        autoFocus
-                    />
-                    <input 
-                        type="text"
-                        placeholder="Amount"
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}
-                    />
-                    <SingleDatePicker 
-                        date={this.state.createdAt}
-                        onDateChange={this.onDateChange}
-                        focused={this.state.calendarFocused}
-                        onFocusChange={this.onFocusChange}
-                        numberOfMonths={1}
-                        isOutsideRange={() => false}
-                    />
-                    <textarea 
-                        placeholder="Add a note for your expense (optional)"
-                        value={this.state.note}
-                        onChange={this.onNoteChange}
-                    >
-                    </textarea>
-                    <button>{this.props.action}</button>
-                </form>
-            </div>
+            <form className="form" onSubmit={this.onSubmit}>
+                {!!this.state.error && <p className="form__error">{this.state.error}</p>}
+                <input 
+                    type="text" 
+                    placeholder="Description" 
+                    className="text-input"
+                    value={this.state.description}
+                    onChange = {this.onDescriptionChange}
+                    autoFocus
+                />
+                <input 
+                    type="text"
+                    placeholder="Amount"
+                    className="text-input"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                />
+                <SingleDatePicker 
+                    date={this.state.createdAt}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+                <textarea 
+                    placeholder="Add a note for your expense (optional)"
+                    className="text-area"
+                    value={this.state.note}
+                    onChange={this.onNoteChange}
+                >
+                </textarea>
+                <div>
+                    <button className="bttn">{this.props.action}</button>
+                </div>
+            </form>
         );
     }
 }
